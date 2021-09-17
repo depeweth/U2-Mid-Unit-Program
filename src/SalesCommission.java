7bdb857aec70b9b40d695fd2eab111f96b01da0e
depeweth000 <depeweth000@warren.k12.in.us>
2021-09-17 12:17:59 -0400

    Unit 2 done

--- src/SalesCommission.java
+++ src/SalesCommission.java
@@ -18,10 +18,70 @@ Total Earnings: $1184.38
 
  */
 
+/* Get my baseline down for what information is needed
+should start on getting outputs out so I can see the awnsers I type in
+start on the calculations that I would need to make it work/keep testing as I am going to make sure that is all works correctly
+get rounding code going for both piece before bring them together
+look back to tax to find how to do the commission compaired with the total sales
+ */
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class SalesCommission {
 
     public static void main(String[] args) {
 
+        double com = Double.parseDouble(JOptionPane.showInputDialog("What is the commission amount"));
+        double TW = Double.parseDouble(JOptionPane.showInputDialog("What is the amount of hours worked"));
+        double WA = Double.parseDouble(JOptionPane.showInputDialog("What is the wage amount"));
+        double TS = Double.parseDouble(JOptionPane.showInputDialog("What is the total sale amount"));
+
+        JOptionPane.showMessageDialog(null, "The total wage is " + totalwage(TW,WA));
+        JOptionPane.showMessageDialog(null, "The commission is " + totalcar(com,TS));
+        JOptionPane.showMessageDialog(null, "The commission is " + total(totalwage(TW,WA),totalcar(com,TS)));
+
+        System.exit(0);
+    }
+
+    public static double commison(double com){
+
+        return com;
+    }
+
+    public static double time(double TW){
+
+        return TW;
+    }
+
+    public static double wage(double WA){
+
+        return WA;
+    }
+
+    public static double sale(double TS){
+
+        return TS;
+    }
+
+    public static double totalwage(double TW, double WA){
+
+        return TW * WA;
+    }
+
+    public static double totalcar(double com, double TS){
+
+        return com * TS;
+    }
+
+    public static double total(double totalwage, double totalcar){
+
+        return totalcar + totalwage;
+    }
+    public static void outPutResults(double total){
+        DecimalFormat round = new DecimalFormat("#,##.00");
+
+        JOptionPane.showMessageDialog(null, "The total is " + round.format(total));
     }
 
 }
